afcf82862644d8419f9385ea4c077ff6285b149e
Andrew Grapsas <andrew.grapsas@socialbon.com>
2013-04-04 11:32:14 -0400

    Multiple include protection for actor enum, added class commenting

--- headers/ActorTypes.h
+++ headers/ActorTypes.h
@@ -1,3 +1,11 @@
+#ifndef __EACTOR_TYPES_H_
+#define __EACTOR_TYPES_H_
+
+/*
+	EActorTypes
+	======================
+	Enum containing the various types of behavior classes to implement
+*/
 enum class EActorTypes
 {
 	HUNTER,		// Hunters intelligently search down prey and consume them
@@ -5,3 +13,5 @@ enum class EActorTypes
 	FOX,		// Foxes are cunning and witty, able to evade
 	SIZE
 };
+
+#endif

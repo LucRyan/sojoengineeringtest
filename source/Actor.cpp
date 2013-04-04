71461e845195e1ce8cc1e2139aec911175e83f80
Andrew Grapsas <andrew.grapsas@socialbon.com>
2013-04-04 11:30:08 -0400

    refining a comment on unit movement and collision

--- source/Actor.cpp
+++ source/Actor.cpp
@@ -2,6 +2,6 @@
 
 const bool Actor::DoesCollide( const Actor* actor )
 {
-	// We assume actors are 1 world unit in width and height
+	// We assume actors are 1 world unit in width and height and move 1 unit per turn
 	return actor->x == x && actor->y == y;
 }

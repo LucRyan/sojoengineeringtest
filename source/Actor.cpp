e576357300695f2490fd486515cf2efa537d7fe4
Andrew Grapsas <andrew.grapsas@socialbon.com>
2013-04-04 11:32:57 -0400

    Added public comment for Actor cpp file

--- source/Actor.cpp
+++ source/Actor.cpp
@@ -1,5 +1,6 @@
 #include "../headers/Actor.h"
 
+/** Public **/
 const bool Actor::DoesCollide( const Actor* actor )
 {
 	// We assume actors are 1 world unit in width and height and move 1 unit per turn
